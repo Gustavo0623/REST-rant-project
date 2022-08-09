da91368708208ad7e33d8542d0ee382c80ada705
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-08-08 17:14:55 -0700

    added places index view

---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function placesIndex () {
+    return (
+        <Def>
+            <main>
+                <h1>Places Index</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = placesIndex

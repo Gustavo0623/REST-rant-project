e6cf5b484027dd5ef83cbbe218e678915345753f
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-08-20 01:31:34 -0700

    rest rant part 6 updated

--- index.js
+++ index.js
@@ -3,18 +3,21 @@
 require('dotenv').config()
 const express = require('express');
 const app = express()
+const methodOverride = require('method-override')
 
 // Express Settings
 
+// Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // controlers + routes
 
 app.use('/places', require('./controllers/places'))
-app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) =>{

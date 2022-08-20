8f1210cc7d3e554994ed38afe0ef41abd024b667
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-08-20 01:13:41 -0700

    rest rant part 6 show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -3,44 +3,45 @@ const places = require('../models/places.js')
 
 // routes
 
-router.get('/new', (req, res) => {
-  res.render('places/new', {places})
-})
-
 router.get('/', (req, res) => {
-    // places array with restaurant data
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/css/images/emy-XoByiBymX20-unsplash.jpg',
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/css/images/ricardo-l-LJmzAGwrUoA-unsplash.jpg',
-      }]
-      
-    res.render('places/index', {places})
+    res.render('places/index')
 })
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  if (!req.body.pic) {
+    if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'https://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
+      req.body.pic = "https://placekitten.com/400/400"
+    }
+    if (!req.body.city) {
     req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
+    }
+    if (!req.body.state) {
     req.body.state = 'USA'
-  }
+    }
   places.push(req.body)
   res.redirect('/places')
 })
+
+router.get('/new', (req, res) => {
+  res.render('places/new', {places})
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
+
 
 
 module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,13 +1,13 @@
 module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/css/images/emy-XoByiBymX20-unsplash.jpg',
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "/css/images/emy-XoByiBymX20-unsplash.jpg",
 }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/css/images/ricardo-l-LJmzAGwrUoA-unsplash.jpg',
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "/css/images/ricardo-l-LJmzAGwrUoA-unsplash.jpg",
 }]

625263ab8fc02303782eea1baad2a5f0136fc419
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-08-27 10:09:06 -0700

    added mongoose, changed models/places and added schema

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+// connect to mongoose
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, // to remove warning messages on terminal (articlle on it https://arunrajeevan.medium.com/understanding-mongoose-connection-options-2b6e73d96de1)
+  useUnifiedTopology: true // to remove warning messages on terminal
+})
+
+module.exports.Place = require('./places')
--- models/places.js
+++ models/places.js
@@ -1,13 +1,12 @@
-module.exports = [{
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/css/images/emy-XoByiBymX20-unsplash.jpg",
-}, {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "/css/images/ricardo-l-LJmzAGwrUoA-unsplash.jpg",
-}]
+const { default: mongoose, Schema } = require("mongoose")
+
+// create new schema
+const placeSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    pic: String,
+    cuisines: {type: String, required: true},
+    city: {type: String, default: 'Anytown'},
+    state: {type: String, default: 'USA'},
+})
+
+module.exports = mongoose.model('Place', placeSchema)

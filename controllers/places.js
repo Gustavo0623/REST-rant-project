13da53f0e87971073fc3e8e6d63b4171ae695e1f
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-08-27 12:47:25 -0700

    validation error message added

--- controllers/places.js
+++ controllers/places.js
@@ -17,11 +17,20 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
-    res.redirect('/places')
+      res.redirect('/places')
   })
   .catch(err => {
-    console.log(err)
-    res.render('error404')
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      res.render('places/new', { message })
+  }
+  else {
+      res.render('error404')
+  }
   })
 })
 

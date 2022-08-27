41900f6c66d092624f4ec5b272b0f6b725e79dd0
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-08-27 12:31:38 -0700

    form validation working as expected

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -31,10 +31,16 @@ function edit_form (data) {
                     <label htmlFor="cuisines">Cuisines</label>
                     <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
                 </div>
-                <div className="form-group">
-                    <label for="founded">Founded Year</label>
-                    <input className="form-control" id="founded" name="founded" />
+                <div className="form-group col-sm-4">
+                    <label htmlFor="founded">Founded Year</label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="founded" 
+                        name="founded" 
+                        value={new Date().getFullYear()} />
                 </div>
+
                 <input className="btn btn-primary" type="submit" value="Save Changes" />
             </form>
           </main>
